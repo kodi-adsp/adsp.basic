3afbeec52b54953280a165335e677b69921ce23d
wsnipex <wsnipex@a1.net>
2015-07-20 18:00:17 +0200

    fix Findlibsamplerate.cmake

--- Findlibsamplerate.cmake
+++ Findlibsamplerate.cmake
@@ -5,18 +5,16 @@
 # LIBSAMPLERATE_INCLUDE_DIRS - the libsamplerate include directory
 # LIBSAMPLERATE_LIBRARIES - The libsamplerate libraries
 
+find_package(PkgConfig)
 if(PKG_CONFIG_FOUND)
-pkg_check_modules (LIBSAMPLERATE libsamplerate)
-list(APPEND SAMPLERATE_INCLUDE_DIRS ${LIBSAMPLERATE_INCLUDEDIR})
+  pkg_check_modules (LIBSAMPLERATE samplerate)
+  list(APPEND LIBSAMPLERATE_INCLUDE_DIRS ${LIBSAMPLERATE_INCLUDEDIR})
 endif()
 
 if(NOT LIBSAMPLERATE_FOUND)
-find_path( LIBSAMPLERATE_INCLUDE_DIRS "samplerate.h"
-           PATH_SUFFIXES "samplerate" )
-
-find_library( LIBSAMPLERATE_LIBRARIES
-              NAMES "libsamplerate"
-              PATH_SUFFIXES "libsamplerate" )
+  find_path( LIBSAMPLERATE_INCLUDE_DIRS "samplerate.h"
+             PATH_SUFFIXES "samplerate" )
+  find_library( LIBSAMPLERATE_LIBRARIES samplerate)
 endif()
 
 # handle the QUIETLY and REQUIRED arguments and set SAMPLERATE_FOUND to TRUE if
